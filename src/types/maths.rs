af44e725ca06a3c55d91d772f582793e215e81e1
Marvin Cano <canomarvin01@gmail.com>
2025-09-24 11:56:10 +0200

    fix: resolve derive macros

--- src/types/maths.rs
+++ src/types/maths.rs
@@ -1,16 +1,14 @@
-use bytemuck::Zeroable;
-
 use crate::types::Pixel;
 
 #[repr(C, align(8))]
-#[derive(Clone, Copy, Debug, Zeroable)]
+#[derive(Clone, Copy, Debug, bytemuck::Zeroable)]
 pub struct Vec2 {
     pub x: f32,
     pub y: f32,
 }
 
 #[repr(C, align(16))]
-#[derive(Clone, Copy, Debug, Zeroable)]
+#[derive(Clone, Copy, Debug, bytemuck::Zeroable)]
 pub struct Vec3 {
     pub x: f32,
     pub y: f32,
@@ -28,7 +26,7 @@ impl From<Pixel> for Vec3 {
 }
 
 #[repr(C)]
-#[derive(Clone, Copy, Debug, Zeroable)]
+#[derive(Clone, Copy, Debug, bytemuck::Zeroable)]
 pub struct Transform {
     pub position: Vec2,
     pub scale: Vec2,
